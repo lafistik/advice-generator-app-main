cceccadca83f9af2f37d61a6324aafbb1e9ac44a
Nech Vladlen <vladlennechaev@gmail.com>
2023-06-04 00:09:23 +0900

    Add styling, logic and get data from api

---
+++ script.js
@@ -0,0 +1,20 @@
+const adviceNum = document.querySelector(".num");
+const adviceText = document.querySelector(".advice__text");
+const generateAdvice = document.querySelector(".dice__container");
+const card = document.querySelector(".card");
+
+window.addEventListener("load", function () {
+  getAdvice();
+  card.style.opacity = "1";
+});
+
+generateAdvice.addEventListener("click", getAdvice);
+
+function getAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      adviceNum.textContent = data.slip.id;
+      adviceText.textContent = data.slip.advice;
+    });
+}
